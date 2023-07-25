24b1ce3dd4669d0638025cbd51cd71639a62b5b1
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-07-25 20:45:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function fibonacci(num) {
-// your code here
-	if(num===1||num===2) return 1
+// your code 
+	if(num===1) return 0
+	if(num===2||num===3) return 1
 	let first=0;
 	let sec=1;
 	let sum=0;
