60a4661588d87040c363162b987b4c33bc1be88e
Prathamesh Kulkarni <65341679+Prathamesh-Kulkarni01@users.noreply.github.com>
2023-07-25 20:40:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function fibonacci(num) {
 // your code here
+	if(num===1||num===2) return 1
 	let first=0;
 	let sec=1;
 	let sum=0;
-	while(num-->2){
+	while(num-->1){
 		sum=first+sec;
 		first=sec;
 		sec=sum;
 	}
 
-	return sum
+	return first
 }
 
 module.exports = fibonacci;
